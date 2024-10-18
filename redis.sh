4a1230dbc70ebe50939f00cdb47ed45f3c2e4fa6
Kavitha <kavithakavi2000reddy@gmail.com>
2024-10-18 14:41:07 +0530

    Roboshop shell script

--- redis.sh
+++ redis.sh
@@ -9,9 +9,8 @@ MONGODB_HOST=mongodb.joinaiops76.online
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log" 
-exec &>$LOGFILE
 
-echo "Script started executing at $TIMESTAMP" 
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
 VALIDATE() {
     if [ $1 -ne 0 ]
     then 
@@ -23,27 +22,27 @@ VALIDATE() {
 }
 if [ $ID -ne 0 ]
 then 
-  echo -e "$R Please run this script with Root access $N"
+  echo -e "$R Please run this script with Root access $N" &>> $LOGFILE
   exit 1
 else
   echo -e "Hyy $G Root user $N"
 fi
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y 
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOGFILE 
 VALIDATE $? "Installing Remi release"
 
-dnf module enable redis:remi-6.2 -y 
+dnf module enable redis:remi-6.2 -y &>> $LOGFILE
 VALIDATE $? "Enabling Redis"
 
-dnf install redis -y 
+dnf install redis -y &>> $LOGFILE
 VALIDATE $? "Installing Redis"
 
-set -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf
+set -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>> $LOGFILE
 VALIDATE $? "Allowing remote connections"
 
-systemctl enable redis 
+systemctl enable redis &>> $LOGFILE
 VALIDATE $? "Enabling Redis"
 
-systemctl start redis 
+systemctl start redis &>> $LOGFILE
 VALIDATE $? "Starting Redis"
 
 
